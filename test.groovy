f5dbc07227e36ee2e57e17fb2e4246b71f76db10
Deepank <deepank@Deepanks-MacBook-Air.local>
2022-11-30 15:59:56 +0530

    changes

--- test.groovy
+++ test.groovy
@@ -4,38 +4,38 @@ def call(String name = 'human') {
   echo "Hello, ${name}."
 }
 
-#import hudson.model.User
-#import hudson.model.Hudson
-#import hudson.security.AuthorizationStrategy
-#import hudson.security.Permission
-#import com.michelin.cio.hudson.plugins.rolestrategy.RoleBasedAuthorizationStrategy
-#import com.michelin.cio.hudson.plugins.rolestrategy.RoleMap
-#
-#AuthorizationStrategy strategy = Hudson.getInstance().getAuthorizationStrategy();
-#
-#jobs = []
-#user = User.current()
-#userId = user.getId()
-#
-#if (strategy != null && strategy instanceof com.michelin.cio.hudson.plugins.rolestrategy.RoleBasedAuthorizationStrategy) {
-#    roleStrategy = (RoleBasedAuthorizationStrategy) strategy;
-#    // not very straightforward to get the groups for a given user
-#    roles = roleStrategy.getGrantedRoles("globalRoles")
-#    for (entry in roles) {
-#        role = entry.key
-#        users = entry.value
-#        if (role.getName().equals("dev")) {
-#            if (userId in users) {
-#                jobs = ["Dev", "Stg"]
-#                break
-#            }
-#        } else if (role.getName().equals("admin")) {
-#            if (userId in users) {
-#                jobs = ["Prod", "Preprod", "Dev", "Stg"]
-#                break
-#            }
-#        }
-#    }
-#}
-#
-#return jobs
+//import hudson.model.User
+//import hudson.model.Hudson
+//import hudson.security.AuthorizationStrategy
+//import hudson.security.Permission
+//import com.michelin.cio.hudson.plugins.rolestrategy.RoleBasedAuthorizationStrategy
+//import com.michelin.cio.hudson.plugins.rolestrategy.RoleMap
+//
+//AuthorizationStrategy strategy = Hudson.getInstance().getAuthorizationStrategy();
+//
+//jobs = []
+//user = User.current()
+//userId = user.getId()
+//
+//if (strategy != null && strategy instanceof com.michelin.cio.hudson.plugins.rolestrategy.RoleBasedAuthorizationStrategy) {
+//    roleStrategy = (RoleBasedAuthorizationStrategy) strategy;
+//    // not very straightforward to get the groups for a given user
+//    roles = roleStrategy.getGrantedRoles("globalRoles")
+//    for (entry in roles) {
+//        role = entry.key
+//        users = entry.value
+//        if (role.getName().equals("dev")) {
+//            if (userId in users) {
+//                jobs = ["Dev", "Stg"]
+//                break
+//            }
+//        } else if (role.getName().equals("admin")) {
+//            if (userId in users) {
+//                jobs = ["Prod", "Preprod", "Dev", "Stg"]
+//                break
+//            }
+//        }
+//    }
+//}
+//
+//return jobs
