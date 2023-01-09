086634fd2d6366ef77452e9253cdcef974ced891
Deepank Verma <dpnkvrm@gmail.com>
2023-01-09 17:28:07 +0530

    dsgabefdvc

--- Jenkinsfile-access.groovy
+++ Jenkinsfile-access.groovy
@@ -2,23 +2,19 @@
 
 pipeline {
     agent any
-    parameters {
-        string(name: "Access_Request_ID", description: "Jira ticket id only")
-        string(name: "User_Name", description: "user email address or username")
-        string(name: "Global_Role_Name", defaultValue: "readonly", trim: true, description: "Global Role name")
-        string(name: "Project_Role_Name", trim: true, description: "Global Role name")
-    }
+
     stages{
         stage("Access Management") {
             steps{
                 script {
                     if (params.Access_Request_ID == "") {
                         error("Please mention Access Request ID.")
-                     }
-                accessManagement.call(
-                    userName: params.User_Name,
-                    roleName: params.Project_Role_Name,
-                    globalroleName: params.Global_Role_Name
+                    }
+
+                    accessManagement(
+                            userName: params.User_Name,
+                            roleName: params.Project_Role_Name,
+                            globalroleName: params.Global_Role_Name
                     )
                 }
             }
--- vars/test2.groovy
+++ vars/test2.groovy
@@ -38,3 +38,22 @@ if (strategy != null && strategy instanceof com.michelin.cio.hudson.plugins.role
 return jobs
 """]]]])])
 }
+
+import hudson.model.User
+import hudson.model.Hudson
+import hudson.security.AuthorizationStrategy
+import hudson.security.Permission
+import com.michelin.cio.hudson.plugins.rolestrategy.RoleBasedAuthorizationStrategy
+import com.michelin.cio.hudson.plugins.rolestrategy.RoleMap
+
+AuthorizationStrategy strategy = Hudson.getInstance().getAuthorizationStrategy();
+
+if (strategy != null && strategy instanceof com.michelin.cio.hudson.plugins.rolestrategy.RoleBasedAuthorizationStrategy) {
+    roleStrategy = (RoleBasedAuthorizationStrategy) strategy;
+    // not very straightforward to get the groups for a given user
+    roles = roleStrategy.getGrantedRoles("projectRoles")
+    for (entry in roles) {
+        role = entry.key
+        println role.getName()
+    }
+}
